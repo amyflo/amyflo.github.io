274a807ea2d50f4447c5410ab476048036d860a8
amyflo <amyflo@github.com>
2020-12-30 23:37:49 -0800

    Designing about / work pages
    - creating about page
    - creating work page
    - desiging home page
    - creating layout
    
    Notes:
    - need to standardize footer across all pages

--- script.js
+++ script.js
@@ -1,58 +1,6 @@
 var selectedCategory;
 
 $('.carousel').carousel()
-  
-window.onload = function () {
-    var $grid = $('#grid').isotope({
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        masonry: {
-            columnWidth: '.grid-sizer'
-        },
-        getSortData: {
-            selectedCategory: function( itemElem ) {
-            return $( itemElem ).hasClass( selectedCategory ) ? 0 : 1;
-            }
-        }
-    });
-    var $items = $('.grid').find('.grid-item');
-
-    $('.sort-button-group').on( 'click', '.nav-button', function() {
-    // set selectedCategory
-    selectedCategory = $( this ).attr('data-category');
-    if ( selectedCategory == 'all' ) {
-        $grid.isotope({
-        sortBy: 'original-order'
-        });
-        // restore all items to full opacity
-        $items.css({
-        opacity: 1
-        });
-        return;
-    }
-    // change opacity for selected/unselected items
-    var selectedClass = '.' + selectedCategory;
-    $items.filter( selectedClass ).css({
-        opacity: 1
-    });
-    $items.not( selectedClass ).css({
-        opacity: 0.25
-    });
-
-    // update sort data now that selectedCategory has changed
-    $grid.isotope('updateSortData');
-    $grid.isotope({ sortBy: 'selectedCategory' });
-    });
-
-    // change is-checked class on buttons
-    $('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $( this ).addClass('is-checked');
-    });
-    });
-}
 
 // List of sentences
 var _CONTENT = [
@@ -254,3 +202,12 @@ function mouseIn() {
   $('.hover-me').hover(mouseIn, mouseOut);
 
   
+
+  function preloader() {
+	myVar = setTimeout(showPage, 1000);
+  }
+
+  function showPage() {
+	// document.getElementById("loader").style.display = "none";
+	document.getElementById("loading").style.display = "block";
+  }
